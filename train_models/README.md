997567e57a62594c4cebe64157e9224951bdc0a8
SriChandana98 <saminenisrichandana@gmail.com>
2023-05-05 23:02:30 -0400

    Commit train_models

---
+++ train_models/README.md
@@ -0,0 +1,10 @@
+## To run
+
+#### 1. Train clean models
+
+```python
+$ cd cifar10 
+$ python train_resnet20.py
+```
+This will create a `models` directory and save the trained model. 
+
---
+++ train_models/cifar10/train_cnn2.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
+import os
+
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+
+# Normalize pixel values to be between 0 and 1
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck']
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(32, 3, padding='same', input_shape=train_images.shape[1:], activation='relu'),
+    tf.keras.layers.Conv2D(32, 3, activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Dropout(0.25),
+
+    tf.keras.layers.Conv2D(64, 3, padding='same', activation='relu'),
+    tf.keras.layers.Conv2D(64, 3, activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Dropout(0.25),
+
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(len(class_names), activation='softmax'),
+])
+
+print(model.summary())
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+history = model.fit(train_images, train_labels, epochs=10, 
+                    validation_data=(test_images, test_labels))
+
+save_dir = './models/'
+model_name = f'cifar10_cnn2_10.h5'
+if not os.path.isdir(save_dir):
+    os.makedirs(save_dir)
+filepath = os.path.join(save_dir, model_name)
+model.save(filepath)
